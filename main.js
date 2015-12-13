db7ff2058053bb5ba686e8f5fe8db32de51d76f0
Kevin Lacker <lacker@gmail.com>
2015-12-13 09:08:16 -0800

    conncet to sphero, drive it around

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 'use strict';
 
+let Cylon = require('cylon');
+
+Cylon.robot({
+  connections: {
+    sphero: { adaptor: 'sphero', port: '/dev/tty.Sphero-POR-AMP-SPP' }
+  },
+
+  devices: {
+    sphero: { driver: 'sphero' }
+  },
+
+  work: function(my) {
+    every((1).second(), () => {
+      my.sphero.roll(60, Math.floor(Math.random() * 360));
+    });
+  }
+}).start();
