dcd057e2d8f361363ed620fc7256bf9c084fdb9b
Kevin Lacker <lacker@gmail.com>
2015-12-17 20:43:40 -0800

    extract information more cleanly from sound card

--- listen.js
+++ listen.js
@@ -12,21 +12,32 @@ for (let i = 0; i < 1024; i++) {
   range.push(i);
 }
 
+let zero = [range.map(x => 0)];
 let soft = [range.map(x => 2)];
 let loud = [range.map(x => 20)];
 
 let i = -1;
 
+function round(num) {
+  let answer = Math.round(num);
+  // Avoid annoying-looking negative zero
+  if (answer === 0) {
+    answer = 0;
+  }
+  return answer;
+}
+
+function get(buffer, i) {
+  return round(buffer[0][i] * 100);
+}
+
 // Input seems to be a 1x24 matrix of numbers.
 // Returns the output buffer to play.
 function processAudio(input) {
   i++;
-  console.log(i, input[0][0]);
-  if (i % 2 == 0) {
-    return soft;
-  } else {
-    return loud;
-  }
+  console.log('----------------');
+  console.log(get(input, 0), get(input, 1), get(input, 2));
+  return zero;
 }
  
 engine.addAudioCallback(processAudio);
